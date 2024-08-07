69046d2b6c95f1e93ebb3db499b951cb96ae3ef7
cw3st <156005055+cw3st@users.noreply.github.com>
2024-08-07 01:46:00 +0100

    Add files via upload

---
+++ hairp.js
@@ -0,0 +1,39 @@
+function play(){
+    let xy = document.getElementById("artii")
+    xy.style.display = "block"
+    let mm = document.getElementById("arti")
+    mm.style.display = "none"
+}
+
+ function make(){
+     let xy = document.getElementById("arti")
+     xy.style.display = "block"
+     let mm = document.getElementById("artii")
+     mm.style.display = "none"
+ }
+function clod() {
+    let xy = document.getElementById("none")
+    xy.style.display = "block"
+}
+ function gron() {
+     let xy = document.getElementById("none")
+     xy.style.display = "none"
+}
+
+function pay(){
+    let xy = document.getElementById("rlist")
+    xy.style.display = "block"
+    let cc = document.getElementById("srnav")
+    cc.style.display = "block"
+    let br = document.getElementById("rnav")
+    br.style.display = "none"
+}
+
+function cod(){
+    let xy = document.getElementById("rlist")
+    xy.style.display = "none"
+    let br = document.getElementById("srnav")
+    br.style.display = "none"
+    let hh = document.getElementById("rnav")
+    hh.style.display ="block"
+}
